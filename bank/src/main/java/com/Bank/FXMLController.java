13caf2ac793a8b85e0f390460e782c0ea8121b1c
SAYEED AJMAL <sayeedajmala06@gmail.com>
2021-05-03 01:26:23 +0530

    removed Label Importation

--- bank/src/main/java/com/Bank/FXMLController.java
+++ bank/src/main/java/com/Bank/FXMLController.java
@@ -25,7 +25,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
